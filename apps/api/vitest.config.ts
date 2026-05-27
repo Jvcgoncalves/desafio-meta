03a5b1f7ed452b1a34c12d05161ca1c1c4a44661
João Vitor Gonçalves da Costa <goncalvesdacosta05@gmail.com>
2026-05-26 22:47:40 -0300

    fix: disable file parallelism when running integration tests

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -6,8 +6,9 @@ export default defineConfig({
     include: ["tests/**/*.test.ts", "tests/**/*.spec.ts"],
     exclude: ["dist/**", "node_modules/**"],
     env: {
-      TEST_DATABASE_URL: "postgresql://casecellshop:casecellshop@localhost:5432/casecellshop?schema=public",
-      RUN_TEST_MIGRATIONS: "1",
-    }
+      TEST_DATABASE_URL: "postgresql://casecellshop:casecellshop@localhost:5432/casecellshop?schema=public", // remove it if want to skip integration tests
+      RUN_TEST_MIGRATIONS: "2",
+    },
+    fileParallelism: true,
   }
 });
